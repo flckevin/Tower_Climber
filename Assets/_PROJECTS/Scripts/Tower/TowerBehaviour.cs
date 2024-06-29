53aee50f050a5a1affc7f63fbb7fb94cad3d803a
quocanhhoang <quocanhbeo3010@gmail.com>
2024-06-29 20:20:58 +0700

    Quoc Anh: Package import

--- Assets/_PROJECTS/Scripts/Tower/TowerBehaviour.cs
+++ Assets/_PROJECTS/Scripts/Tower/TowerBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TowerBehaviour : MonoBehaviour
@@ -7,7 +8,9 @@ public class TowerBehaviour : MonoBehaviour
     [Header("GENERAL INFO")]
     public float checkRadius; //radius of tower to check
     public LayerMask mask; //mask check
-    private List<GameObject> enemy = new List<GameObject>(); // list to store all enmey
+
+    private List<EntitiesCore> enemy = new List<EntitiesCore>(); // list to store all enmey
+    private EntitiesCore focusTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +19,34 @@ public class TowerBehaviour : MonoBehaviour
 
     void Update()
     {
-       Collider[] hitColliders = Physics.OverlapSphere(this.gameObject.transform.position, 
-                                                        checkRadius,
-                                                        mask);
-        
-        if(hitColliders.Length > 0)
+        TowerShootBehaviour();
+    }
+
+    private void TowerShootBehaviour() 
+    {
+        //if enemy count is less than 0 then dont execute
+        if (enemy.Count < 0) return;
+
+        //if focus target not exist
+        if (focusTarget == null)
         {
-            
+            //get focus target
+            focusTarget = enemy[0];
+        }
+
+        
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check if object enter to collider has enemy tag
+        if (other.CompareTag("Enemy")) 
+        {
+            //add that to enemy list
+            enemy.Add(other.GetComponent<EntitiesCore>());
         }
     }
+
 }
