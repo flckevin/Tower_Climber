b78eaf29b5b5d773861cf7ed5cd5edd27269d879
quocanhhoang <quocanhbeo3010@gmail.com>
2024-07-11 00:10:09 +0700

    Quoc Anh: Spawner - WIP

--- Assets/_PROJECTS/Scripts/Entities/Utilities/EntitiesSpawner.cs
+++ Assets/_PROJECTS/Scripts/Entities/Utilities/EntitiesSpawner.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class EntitiesSpawner : MonoBehaviour
 {
+    [Header("SpawnerInfo"), Space(10)]
+    public int amountPerEnemy; // amount per enemy that going to be in scene
+    public int amountBloodParticlePrefab; //amount of blood particle that going to be on scene
+    public float randSphereRadius; // sphere radius
+
+    [Header("Entity"),Space(10)]
     public int maxmimumEnemyAmount; // maximum amount of enemy that can spawn during current wave
     public float delayBetween; // delay between waves
     public List<EntitiesCore> entities;//list of all avalible entity in game
-   
+
+    [Header("Entity Ultility"), Space(10)]
+    public ParticleSystem bloodParticlePrefab;//blood particle prefab
+
     private int spawnedAmount; // spawn amount
     private Coroutine _spawnCou; // spawn couroutine
     // Start is called before the first frame update
     void Start()
     {
         //===================== SET =====================
+       
         //loop all avalible entity
         for (int i =0; i < entities.Count; i++) 
         {
             //spawn each entity 20 times
-            PoolManager.Setup(entities[i],20);
+            PoolManager.Setup(entities[i], amountPerEnemy);
         }
+
+        //setup pool of blood particle prefab
+        PoolManager.Setup(bloodParticlePrefab, amountBloodParticlePrefab);
+
         //start spawning
         StartSpawning();
         //===================== SET =====================
@@ -47,10 +61,22 @@ public class EntitiesSpawner : MonoBehaviour
     {
 
         while (spawnedAmount < maxmimumEnemyAmount) 
-        { 
-            
+        {
+            Vector3 _spawnPos = randomSpawnPoint();
+
             yield return new WaitForSeconds(delayBetween);  
         }
 
+    }
+
+    private Vector3 randomSpawnPoint() 
+    {
+        Vector3 _spawnPos;
+
+        Vector3 _spawnDir = Random.onUnitSphere*randSphereRadius; // could be anywhere on a sphere
+        _spawnDir.y = 0;
+        _spawnPos = _spawnDir;
+        _spawnPos.Normalize();
+        return _spawnPos;
     }
 }
